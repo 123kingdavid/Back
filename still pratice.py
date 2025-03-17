cc4ff39f01abfe279b1d58b631a3d64acccc90f7
kingdaviddesmond <kddesmond99@gmail.com>
2025-03-17 10:52:46 +0100

    back from break

---
+++ still pratice.py
@@ -0,0 +1,24 @@
+# def hello(greeting,title,first,last):
+#     print(f"{greeting} {title} {first} {last}")
+
+
+# hello("Hello","Mr","king davis", "desmond",)
+
+      
+
+# def get_phone(country,area,first,last):
+#     return f"{country} -{area} - {first} - {last}"
+
+# phone_num = get_phone(country= 2, area= 234, first=231,last= 7201,)
+
+# print(phone_num) 
+
+# args: allows you to pass multiple non key arguments
+# kwargs: allows you to pass multiple keyword arguments
+
+def add(*args):
+    total = 0
+    for arg in args:
+        total += arg
+    return total  
+print(add(1,2,3))  
